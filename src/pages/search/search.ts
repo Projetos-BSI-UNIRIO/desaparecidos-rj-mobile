c24bf5453eede3b0d80d0f29e50c9e906e46e09a
Thais Simoes <thais.simoes@uniriotec.br>
2017-07-24 21:33:14 -0300

    Bugs consertados e envio de json funcionando.

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -8,12 +8,98 @@ import { NavController, NavParams } from 'ionic-angular';
   entryComponents: [List]
 })
 export class SearchPage {
-  public nome; mae; pai; desaparecimento;
+  public nomeCompleto=""; mae=""; pai=""; idade; altura; sexo; corPele; corCabelo; corOlhos; tipoFisico; infoAdicionaisAparencia; tatuagem; cicatriz; amputado; deficiente; 
+
+
 
 constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-goToListOfResults(){
-  this.navCtrl.push(List);
+ 
+
+tratamentoInfoAdicionais(){
+  if(this.infoAdicionaisAparencia != null){
+  for(var i=0; i<this.infoAdicionaisAparencia.length; i++){
+    
+      if(this.infoAdicionaisAparencia[i]=="Tatuagem"){
+        this.tatuagem = true;
+      }
+      else{
+        this.tatuagem=false;
+      }
+      if(this.infoAdicionaisAparencia[i]=="Cicatriz"){
+        this.cicatriz = true;
+      }
+      else{
+        this.cicatriz=false;
+      }
+      if(this.infoAdicionaisAparencia[i]=="Deficiente"){
+        this.deficiente = true;
+      }
+      else{
+        this.deficiente=false;
+      }
+      if(this.infoAdicionaisAparencia[i]=="Amputado"){
+        this.amputado = true;
+      }
+      else{
+        this.amputado=false;
+      }
+    }
+  }
 }
+dadosDesaparecido(){
+    this.tratamentoInfoAdicionais();
+    this.tratamentoCamposTexto();
+
+  var dadosDoDesaparecido ={
+    "nome": this.nomeCompleto,
+    "nome_pai": this.pai,
+    "nome_mae": this.mae,
+    "idade_aparente": this.idade,
+    "possui_cicatriz": this.cicatriz,
+    "possui_tatuagem": this.tatuagem,
+    "possui_deficiencia": this.deficiente,
+    "sofreu_amputacao": this. amputado,
+    "tipo_fisico": this.tipoFisico,
+    "cor_olhos": this.corOlhos,
+    "cor_pele": this.corPele,
+    "cor_cabelo": this.corCabelo,
+    "faixa_altura":this.altura
+    
+}
+
+  return dadosDoDesaparecido;
+}
+
+tratamentoCamposTexto(){
+  if(this.nomeCompleto!=""){
+    this.nomeCompleto.trim()
+  }
+    
+  if(this.mae!=""){
+    this.mae.trim();
+  }
+ 
+  if(this.pai!=""){
+    this.pai.trim();
+  }
+  
+
+}
+
+envioDeDadosParaOServidor(){
+  var xhr = new XMLHttpRequest();
+  var url = "http://104.131.39.194:8000/webserver/desaparecidos/buscarDesaparecido/?dados=" + encodeURIComponent(JSON.stringify(this.dadosDesaparecido()));
+  xhr.open("GET", url, true);
+  xhr.setRequestHeader("Content-type", "application/json");
+  xhr.send();
+}
+
+
+irAListaDeResultados(){
+  
+  this.envioDeDadosParaOServidor();
+  this.navCtrl.push(List);
+  }
 }
