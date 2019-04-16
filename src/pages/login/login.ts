ded1045ec44a1bf5c0989aaf31ebb70cc7e55419
henriquecefet <rodriguesoareshenrique@gmail.com>
2019-04-16 14:36:08 -0300

    Funcionando

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { WebApiService } from '../../providers/web-api-service';
 import { HomePage } from '../home/home';
-
+import { Http, Headers } from '@angular/http';
+import { URLSearchParams } from '@angular/http';
 
 @Component({
   selector: 'page-login',
@@ -13,7 +14,7 @@ import { HomePage } from '../home/home';
 export class LoginPage {
   public username; password; autenticacao; user;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private webapi: WebApiService, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private webapi: WebApiService, public loadingCtrl: LoadingController, public http: Http) {
 
   }
   montaURL(user){
@@ -31,8 +32,19 @@ export class LoginPage {
 
 
   logar(){ 
+    let urlSearchParams = new URLSearchParams();
+    urlSearchParams.append('username', this.username);
+    urlSearchParams.append('password', this.password);
+    this.http.post('https://desaparecidos-rj-web1.herokuapp.com/webserver/loginmobile/', urlSearchParams).subscribe(
+      data => {
+        this.navCtrl.push(HomePage);
+      },
+      err => {
+        alert("Usuário ou senha incorretos");
+      }, () => {});
+    /*
     this.webapi.enviarDadosServidor(this.montaURL(this.dadosUsuario())).subscribe( //monta URL com os dados do Json
-      data => { //se não houver erro, entrará nesse bloco de comando
+      data => { //se não houver erro, entrará nesse bloco de comandoz
         this.navCtrl.push(HomePage);
 
       }, err => { //em caso de erro, entrará nesse bloco de comando
@@ -40,5 +52,6 @@ export class LoginPage {
          alert(err);
       }, () => {}
     );
+    */
   }
 }
--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -3,6 +3,8 @@ import { List } from '../list/list';
 import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { NoResultsPage } from '../no-results/no-results';
 import { WebApiService } from '../../providers/web-api-service';
+import { Http, Headers } from '@angular/http';
+import { URLSearchParams } from '@angular/http';
 
 @Component({
   selector: 'page-list',
@@ -15,7 +17,7 @@ export class SearchPage {
   public nomeCompleto=""; mae=""; pai=""; idade; altura; sexo; corPele; corCabelo; corOlhos; tipoFisico; infoAdicionaisAparencia; tatuagem; cicatriz; amputado; deficiente; url;
   public pessoas; desabilitarBotaoBuscar;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private webapi: WebApiService, public loadingCtrl: LoadingController) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private webapi: WebApiService, public loadingCtrl: LoadingController, public http: Http) {
   }
  /**
   * Tratamento de informações do campo "Informações adicionais" para criação do json
@@ -108,7 +110,7 @@ export class SearchPage {
   * @param jsonDeEntrada : Json com os dados de busca inseridos pelo usuário
   */
   montaURL(jsonDeEntrada) {
-  return "http://desaparecidos-rj-web1.herokuapp.com/webserver/desaparecidos/buscarDesaparecido/?dados=" + encodeURIComponent(JSON.stringify(jsonDeEntrada));}
+  return "http://desaparecidos-rj-web1.herokuapp.com/webserver/desaparecidos/buscarDesaparecido/?" + encodeURIComponent(JSON.stringify(jsonDeEntrada));}
 
     /**
      * Método que recebe os resultados do servidor
@@ -116,7 +118,40 @@ export class SearchPage {
 
   recebendoResultado(){    // Call API to get people searched   
     this.desabilitarBotaoBuscar=true;
-
+    let dados = this.dadosDesaparecido()
+    let urlSearchParams = new URLSearchParams();
+    /*
+    urlSearchParams.append('nome', this.nomeCompleto);
+    urlSearchParams.append('nome_pai', this.pai);
+    urlSearchParams.append('nome_mae', this.mae);
+    urlSearchParams.append('idade_aparente', this.idade);
+    urlSearchParams.append('possui_cicatriz', this.cicatriz);
+    urlSearchParams.append('possui_tatuagem', this.tatuagem);
+    urlSearchParams.append('possui_deficiencia', this.deficiente);
+    urlSearchParams.append('sofreu_amputacao', this. amputado);
+    urlSearchParams.append('cor_olhos', this.corOlhos);
+    urlSearchParams.append('cor_pele', this.corPele);
+    urlSearchParams.append('cor_cabelos': this.corCabelo);
+    urlSearchParams.append('faixa_altura',this.altura);
+    urlSearchParams.append('sexo', this.sexo);
+    */
+    urlSearchParams.append('dados', JSON.stringify(dados) );
+    this.http.post('http://desaparecidos-rj-web1.herokuapp.com/webserver/desaparecidos/buscarDesaparecido/', urlSearchParams).subscribe(
+      data => { //se não houver erro, entrará nesse bloco de comando
+        this.pessoas = data.json();
+        this.pessoas= this.pessoas.desaparecidos;
+        if(this.pessoas.length==0){
+          this.navCtrl.push(NoResultsPage);
+        }       
+        else{
+          this.navCtrl.push(List, {"pessoa": this.pessoas});
+        }
+      }, err => { //em caso de erro, entrará nesse bloco de comando
+        this.navCtrl.push(NoResultsPage);
+        // alert(err);
+      }, () => {}
+      );
+    /*
     this.webapi.enviarDadosServidor(this.montaURL(this.dadosDesaparecido())).subscribe( //monta URL com os dados do Json
       data => { //se não houver erro, entrará nesse bloco de comando
         this.pessoas = data.json();
@@ -132,6 +167,7 @@ export class SearchPage {
         // alert(err);
       }, () => {}
     );
+    */
   }
 
   /**
